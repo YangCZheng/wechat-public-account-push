e1ac187fe10b3ce0b9ff44a67c1456befcbb50e9
YangCZheng <118680225+YangCZheng@users.noreply.github.com>
2022-11-21 00:27:13 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,22 +8,22 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx680ec4fb24c06e4d',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: 'f4d92d49356ab2fa15d2fb54bd932c8e',
 
   PROVINCE: '广东',
-  CITY: '惠州',
+  CITY: '东莞',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '啊正',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oq-5v6UijMHL65quu1pe-CvpwDfc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: ' l-zhLR_TDcqiGyNPxTUpsStncVqfc8xmjiBRol3Jeoo',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
